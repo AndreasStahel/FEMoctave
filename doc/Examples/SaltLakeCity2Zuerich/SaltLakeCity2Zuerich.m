5e49d6fc1e44b148b4a982944859e9089a474b67
Andreas Stahel <andreas.stahel@gmx.com>
2023-07-17 17:11:27 +0200

    FEMoctave v2.1.1

--- doc/Examples/SaltLakeCity2Zuerich/SaltLakeCity2Zuerich.m
+++ doc/Examples/SaltLakeCity2Zuerich/SaltLakeCity2Zuerich.m
@@ -11,29 +11,28 @@ interval = linspace(Angles_SLC(1),Angles_ZH(1),N);
 
 figure(1); plot(phi/pi*180,90-theta/pi*180); xlabel('\phi [deg]'); ylabel('90-\theta [deg]'); hold on
 x = sin(theta).*cos(phi); y = sin(theta).*sin(phi); z = cos(theta);
+Dtheta = FEM1DEvaluateDu(phi,theta);
 figure(2); plot3(x,y,z);
            xlabel('x'); ylabel('y'); zlabel('z'); view([-20,10]); hold on
-disp(sprintf("Iteration 1: L = %f km",
-              R*trapz(phi,sqrt(sin(theta).^2+Dtheta.^2))))
-Dtheta = FEM1DEvaluateDu(phi,theta);
+disp(sprintf("Iteration 1: L = %f km",R*trapz(phi,sqrt(sin(theta).^2+Dtheta.^2))))
 figure(3); plot(phi*180/pi,[x,y,z]*n);
            xlabel('\phi [deg]'); hold on;
 phiG = FEM1DGaussPoints(phi);
-for jj = 2:6;
-  Dtheta = FEM1DEvaluateDu(phi,theta);
-  disp(sprintf("Iteration %i: L = %f km",
-              jj,R*trapz(phi,sqrt(sin(theta).^2+Dtheta.^2))))
+for jj = 2:5;
   [thetaG,DthetaG] = pwquadinterp(phi,theta,phiG);
   a = (sin(thetaG).^2 + DthetaG.^2).^(-1/2);
   d = -sin(thetaG).*cos(thetaG)./sqrt(sin(thetaG).^2 + DthetaG.^2);
   [phi,theta] = BVP1D(interval,a,0,0,d,1,BCleft,BCright);
+  Dtheta = FEM1DEvaluateDu(phi,theta);
+  disp(sprintf("Iteration %i: L = %f km",
+              jj,R*trapz(phi,sqrt(sin(theta).^2+Dtheta.^2))))
   figure(1); plot(phi/pi*180,90-theta/pi*180);
   x = sin(theta).*cos(phi); y = sin(theta).*sin(phi); z = cos(theta);
   figure(2); plot3(x,y,z);
   figure(3); plot(phi*180/pi,[x,y,z]*n);
-  pause(1)
+  pause(0.2)
 endfor %% jj
-figure(1); legend('1','2','3','4','5','6'); hold off;
+figure(1); legend('1','2','3','4','5'); hold off;
 figure(2); hold off; figure(3); hold off
 
 
