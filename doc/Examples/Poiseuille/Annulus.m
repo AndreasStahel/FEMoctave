8365c16e6602e8a211d99680b00584dd7f7e7c47
Andreas Stahel <andreas.stahel@gmx.com>
2025-03-03 17:20:36 +0100

    Poisseuille flow

---
+++ doc/Examples/Poiseuille/Annulus.m
@@ -0,0 +1,11 @@
+G = 1; mu = 1; R1 = 1; R2 = 2;
+Interval = linspace(R1,R2,21)';
+[r,u] = BVP1D(Interval,@(r)r,0,0,1,@(r)G/mu*r,0,0);
+
+figure(1); plot(r,u); xlabel( 'radius r'); ylabel('velocity u')
+
+u_exact = G/(4*mu)*((R1^2-r.^2)+(R2^2-R1^2)*ln(r/R1)/ln(R2/R1));
+figure(2); plot(r,u-u_exact); xlabel( 'radius r'); ylabel('u-u_{exact}')
+
+Flow = trapz(r,2*pi*r.*u)
+
---
+++ doc/Examples/Poiseuille/Annulus2Doffset.m
@@ -0,0 +1,15 @@
+G = 1; mu = 1; R1 = 1; R2 = 2;
+angles = linspace(0,2*pi,201)'; angles = angles(1:end-1);
+xy = [R2*cos(angles),R2*sin(angles),-ones(size(angles))];
+Hole.name   = 'Hole';
+Hole.border = [-0.25*R1+R1*cos(angles),R1*sin(angles),-ones(size(angles))];
+Hole.point  = [0,0];
+Mesh = CreateMeshTriangle('Annulus',xy,0.01,Hole);
+Mesh = MeshUpgrade(Mesh,'quadratic');
+
+u = BVP2D(Mesh,1,0,0,0,G/mu,0,0,0);
+
+figure(1); FEMtrimesh(Mesh,u);    xlabel('x'); ylabel('y'); zlabel('velocity u')
+figure(2); FEMtricontour(Mesh,u); xlabel('x'); ylabel('y'); axis equal
+
+Flow = FEMIntegrate(Mesh,u)
