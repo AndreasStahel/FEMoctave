6ef32e7560affef1c662f97afd8a02f89256b41b
Andreas Stahel <andreas.stahel@gmx.com>
2026-05-26 13:45:58 +0200

    Helium ground state

---
+++ doc/Examples/Schroedinger/HeliumGroundState.m
@@ -0,0 +1,22 @@
+pkg load femoctave
+R = 100; N = 40;
+grid = exp(4*linspace(0,1,N))-1;
+grid = R*grid/max(grid);
+
+FEMmesh = CreateMeshRect(grid,grid,-1,-1,-1,-1);
+if 0  FEMmesh = MeshUpgrade(FEMmesh,'quadratic');
+else  FEMmesh = MeshUpgrade(FEMmesh,'cubic');
+endif
+
+a = 1;
+b = @(xy) (1*min(xy,[],2)-2*sum(xy,2))./(xy(:,1).*xy(:,2));
+%% eigenvalues closest to -2, i.e. most negative
+[Eval,Evec] = BVP2Deig(FEMmesh,a,b(FEMmesh.GP),1,0,2,'mode',-2);
+Eval
+
+figure(1); FEMtrimesh(FEMmesh,Evec(:,1))
+           xlabel('\rho_1'); ylabel('\rho_2')
+           xlim([0,8]); ylim([0,8])
+figure(2); FEMtricontour(FEMmesh,Evec(:,1),21)
+           xlabel('\rho_1'); ylabel('\rho_2')
+
---
+++ doc/Examples/Schroedinger/HeliumGroundStateTriangle.m
@@ -0,0 +1,22 @@
+N = 40;  %% select size of grid
+function xy_new = Deform(xy)
+  xy_new = exp(4*xy)-1;
+  xy_new = xy_new/max(xy_new(:))*100;  %% R = 100
+endfunction
+FEMmesh = CreateMeshTriangle('Tria',[0,0,-1;1,0,-1;1,1,-2],0.4*(1/N)^2);
+FEMmesh = MeshDeform(FEMmesh,'Deform');
+
+if 0  FEMmesh = MeshUpgrade(FEMmesh,'quadratic');
+else  FEMmesh = MeshUpgrade(FEMmesh,'cubic');
+endif
+
+a = 1; b = @(xy) -(2*xy(:,1)+xy(:,2))./(xy(:,1).*xy(:,2));
+[Eval,Evec] = BVP2Deig(FEMmesh,a,b(FEMmesh.GP),1,0,1,'mode',-2);
+Eval
+
+[Max,MaxInd] = max(abs(Evec));  %% assure positive maximal value
+Evec = sign(Evec(MaxInd))*Evec;
+figure(101); FEMtrimesh(FEMmesh, Evec);       xlabel('\rho_1'); ylabel('\rho_2')
+             xlim([0,8]); ylim([0,8])
+figure(102); FEMtricontour(FEMmesh, Evec,21); xlabel('\rho_1'); ylabel('\rho_2')
+             xlim([0,8]); ylim([0,8]); axis equal
